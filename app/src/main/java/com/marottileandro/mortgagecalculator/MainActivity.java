e4bd6ab4b443a164c2429716659cdf2fb314aeb2
leandromarotti <leandromarotti@gmail.com>
2020-09-03 13:03:36 -0400

    Initial commit

---
+++ app/src/main/java/com/marottileandro/mortgagecalculator/MainActivity.java
@@ -0,0 +1,45 @@
+package com.marottileandro.mortgagecalculator;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    public static Mortgage mortgage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mortgage = new Mortgage(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStart(){
+        super.onStart();
+        updateView();
+    }
+
+    public void updateView(){
+        TextView amountTV = (TextView) findViewById(R.id.amount);
+        amountTV.setText(mortgage.getFormattedAmount());
+        TextView yearsTV = (TextView) findViewById(R.id.years);
+        yearsTV.setText("" + mortgage.getYears());
+        TextView rateTV = (TextView) findViewById(R.id.rate);
+        rateTV.setText(100 * mortgage.getRate() + "%");
+        TextView monthlyTV = (TextView) findViewById(R.id.payment);
+        monthlyTV.setText(mortgage.formattedMonthlyPayment());
+        TextView totalTV = (TextView) findViewById(R.id.total);
+        totalTV.setText(mortgage.formattedTotalPayment());
+    }
+
+    public void modifyData(View v){
+        Intent myIntent = new Intent(this, DataActivity.class);
+        this.startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_from_left, 0);
+    }
+
+}
